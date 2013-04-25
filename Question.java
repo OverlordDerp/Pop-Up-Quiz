8160d1c1c56d4c3819ac15cde757ceed3888b975
OverlordDerp <aravind_vijayaragavan@hotmail.com>
2013-04-25 09:07:14 -0400

    Revert "Change to Question class (problems with back issues)"
    This reverts commit 323967b8ae031ec47c2760f3351de38ab2be0a41.

--- Question.java
+++ Question.java
@@ -1,22 +1,14 @@
 public class Question {
-String ask, choice1, choice2, choice3, choice4;	
+String ask;	
 
-	    public Question(String q, String C1, String C2, String C3, String C4)
+	    public Question(String q)
 	    {
 	    	ask = q;
-	    	choice1 = C1;
-	    	choice2 = C2;
-	    	choice3 = C3;
-	    	choice4 = C4;
 	    }
 	    
 	    public String getQuestion()
 	    {
 	        return ask;
-	    }
-	    public String toString()
-	    {
-	    	return (ask + ":\n1." + choice1 + "\n2." + choice2 + "\n3." + choice3 + "\n4." + choice4);
 	    }
 	} // main method
 
